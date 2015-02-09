6a9bf2335cece869f1c746278e68be62646e4f00
Hang Su <suhang@icsi.berkeley.edu>
2015-02-09 11:18:45 -0800

    add cmd and path file

---
+++ egs/babel/s5b/cmd_slurm.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export train_cmd="myutils/slurm.pl"
+export decode_cmd="myutils/slurm.pl"
+export cuda_cmd="myutils/slurm.pl -l gpu=1"
---
+++ egs/babel/s5b/cmd_slurmgen.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+export train_cmd="myutils/slurmgen.pl"
+export decode_cmd="myutils/slurmgen.pl"
